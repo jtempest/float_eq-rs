ea3ff5128a608cba8ce61fcc367214420c6ec635
jtempest <54986678+jtempest@users.noreply.github.com>
2020-04-10 14:28:49 +0100

    FloatDiff<Rhs> documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -332,12 +332,73 @@ use core::fmt;
 /// let ulps_diff = a.ulps_diff(&b);
 /// assert_eq!(ulps_diff.re, 1);
 /// assert_eq!(ulps_diff.im, 15);
-///
 /// ```
 ///
 /// If your type does *not* already have an underlying implementation of `FloatDiff`
 /// for its members, then you'll need to take a close look at the descriptions of
 /// the algorithms on a member by member basis.
+///
+/// ## How can I implement `FloatDiff` between two different types?
+///
+/// The type you can `diff` with is controlled by `FloatDiff`'s parameter. Following
+/// on from our previous example, if we wanted to treat `f32` as a complex number
+/// with no imaginary component:
+///
+/// ```rust
+/// # use float_eq::FloatDiff;
+/// struct MyComplex32 {
+///     re: f32,
+///     im: f32,
+/// }
+///
+/// struct MyComplex32UlpsDiff {
+///     re: u32,
+///     im: u32,
+/// }
+///
+/// impl FloatDiff<f32> for MyComplex32 {
+///     type AbsDiff = MyComplex32;
+///     type UlpsDiff = MyComplex32UlpsDiff;
+///
+///     fn abs_diff(&self, other: &f32) -> Self::AbsDiff {
+///         MyComplex32 {
+///             re: self.re.abs_diff(other),
+///             im: self.im.abs_diff(&0.0),
+///         }
+///     }
+///
+///     fn ulps_diff(&self, other: &f32) -> Self::UlpsDiff {
+///         MyComplex32UlpsDiff {
+///             re: self.re.ulps_diff(other),
+///             im: self.im.ulps_diff(&0.0),
+///         }
+///     }
+/// }
+///
+/// impl FloatDiff<MyComplex32> for f32 {
+///     type AbsDiff = <MyComplex32 as FloatDiff<f32>>::AbsDiff;
+///     type UlpsDiff = <MyComplex32 as FloatDiff<f32>>::UlpsDiff;
+///
+///     fn abs_diff(&self, other: &MyComplex32) -> Self::AbsDiff {
+///         other.abs_diff(self)
+///     }
+///
+///     fn ulps_diff(&self, other: &MyComplex32) -> Self::UlpsDiff {
+///         other.ulps_diff(self)
+///     }
+/// }
+///
+/// let a = 1.0000001_f32;
+/// let b = MyComplex32 { re: 1.0, im: 2.0000036, };
+///
+/// let abs_diff = a.abs_diff(&b);
+/// assert_eq!(abs_diff.re, 0.00000011920929);
+/// assert_eq!(abs_diff.im, 2.0000036);
+///
+/// let ulps_diff = a.ulps_diff(&b);
+/// assert_eq!(ulps_diff.re, 1);
+/// assert_eq!(ulps_diff.im, 1_073_741_839);
+/// ```
 ///
 /// ## Examples
 ///
