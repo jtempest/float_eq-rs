e4e9381722456cb5713d0f95d0b7d8b5ffd8b1c1
jtempest <54986678+jtempest@users.noreply.github.com>
2020-06-02 15:47:07 +0100

    More comprehensive tests for blanket ref impls.

--- tests/unit_tests/refs.rs
+++ tests/unit_tests/refs.rs
@@ -1,84 +1,116 @@
-// The commented variants in these tests require specialisation or the traits to
-// be implemented differently in other ways.
-
-use float_eq::assert_float_eq;
+use float_eq::{assert_float_eq, FloatDiff, FloatEqAllDebug, FloatEqDebug};
 
 #[test]
-fn refs() {
-    //assert_float_eq!(1.0, &2.0, abs <= 1.0);
-    //assert_float_eq!(1.0, &2.0, abs_all <= 1.0);
-    //assert_float_eq!(&1.0, 2.0, abs <= 1.0);
-    //assert_float_eq!(&1.0, 2.0, abs_all <= 1.0);
-    assert_float_eq!(&1.0, &2.0, abs <= 1.0);
-    assert_float_eq!(&1.0, &2.0, abs_all <= 1.0);
-    //assert_float_eq!(&1.0, &2.0, abs <= &1.0);
-    //assert_float_eq!(&1.0, &2.0, abs_all <= &1.0);
+fn refs_float_diff() {
+    let a = &1.0f32;
+    let b = &1.5f32;
+    let mut ma = &mut 1.0f32;
+    let mut mb = &mut 1.5f32;
+    assert_eq!(FloatDiff::abs_diff(&a, &b), 0.5);
+    assert_eq!(FloatDiff::abs_diff(&mut ma, &b), 0.5);
+    assert_eq!(FloatDiff::abs_diff(&a, &mut mb), 0.5);
+    assert_eq!(FloatDiff::abs_diff(&mut ma, &mut mb), 0.5);
 
-    let a = 1.0;
-    //assert_float_eq!(a, &1.0, abs <= 1.0);
-    //assert_float_eq!(a, &1.0, abs_all <= 1.0);
-    //assert_float_eq!(&a, 1.0, abs <= 1.0);
-    //assert_float_eq!(&a, 1.0, abs_all <= 1.0);
-    assert_float_eq!(&a, &1.0, abs <= 1.0);
-    assert_float_eq!(&a, &1.0, abs_all <= 1.0);
-    //assert_float_eq!(&a, &1.0, abs <= &1.0);
-    //assert_float_eq!(&a, &1.0, abs_all <= &1.0);
+    let c = &1.000_000_2f32;
+    let mut mc = &mut 1.000_000_2f32;
+    assert_eq!(FloatDiff::ulps_diff(&a, &c), Some(2));
+    assert_eq!(FloatDiff::ulps_diff(&mut ma, &c), Some(2));
+    assert_eq!(FloatDiff::ulps_diff(&a, &mut mc), Some(2));
+    assert_eq!(FloatDiff::ulps_diff(&mut ma, &mut mc), Some(2));
+}
 
-    let b = 2.0;
-    //assert_float_eq!(a, &b, abs <= 1.0);
-    //assert_float_eq!(a, &b, abs_all <= 1.0);
-    //assert_float_eq!(&a, b, abs <= 1.0);
-    //assert_float_eq!(&a, b, abs_all <= 1.0);
+#[test]
+fn refs_float_eq() {
+    let a = &1.0f32;
+    let b = &1.5f32;
+    let mut ma = &mut 1.0f32;
+    let mut mb = &mut 1.5f32;
     assert_float_eq!(&a, &b, abs <= 1.0);
+    assert_float_eq!(&mut ma, &b, abs <= 1.0);
+    assert_float_eq!(&a, &mut mb, abs <= 1.0);
+    assert_float_eq!(&mut ma, &mut mb, abs <= 1.0);
     assert_float_eq!(&a, &b, abs_all <= 1.0);
-    //assert_float_eq!(&a, &b, abs <= &1.0);
-    //assert_float_eq!(&a, &b, abs_all <= &1.0);
+    assert_float_eq!(&mut ma, &b, abs_all <= 1.0);
+    assert_float_eq!(&a, &mut mb, abs_all <= 1.0);
+    assert_float_eq!(&mut ma, &mut mb, abs_all <= 1.0);
+
+    let c = &1.000_000_2f32;
+    let mut mc = &mut 1.000_000_2f32;
+    let eps = 2.0 * f32::EPSILON;
+    assert_float_eq!(&a, &c, rel <= eps);
+    assert_float_eq!(&mut ma, &c, rel <= eps);
+    assert_float_eq!(&a, &mut mc, rel <= eps);
+    assert_float_eq!(&mut ma, &mut mc, rel <= eps);
+    assert_float_eq!(&a, &c, rel_all <= eps);
+    assert_float_eq!(&mut ma, &c, rel_all <= eps);
+    assert_float_eq!(&a, &mut mc, rel_all <= eps);
+    assert_float_eq!(&mut ma, &mut mc, rel_all <= eps);
+
+    assert_float_eq!(&a, &c, ulps <= 2);
+    assert_float_eq!(&mut ma, &c, ulps <= 2);
+    assert_float_eq!(&a, &mut mc, ulps <= 2);
+    assert_float_eq!(&mut ma, &mut mc, ulps <= 2);
+    assert_float_eq!(&a, &c, ulps_all <= 2);
+    assert_float_eq!(&mut ma, &c, ulps_all <= 2);
+    assert_float_eq!(&a, &mut mc, ulps_all <= 2);
+    assert_float_eq!(&mut ma, &mut mc, ulps_all <= 2);
 }
 
 #[test]
-#[allow(unused_mut)]
-fn mutable() {
-    let mut ma = 1.0;
-    let mut mb = 2.0;
-    assert_float_eq!(ma, mb, abs <= 1.0);
-    assert_float_eq!(ma, mb, abs_all <= 1.0);
-    //assert_float_eq!(&ma, mb, abs <= 1.0);
-    //assert_float_eq!(&ma, mb, abs_all <= 1.0);
-    //assert_float_eq!(ma, &mb, abs <= 1.0);
-    //assert_float_eq!(ma, &mb, abs_all <= 1.0);
-    assert_float_eq!(&ma, &mb, abs <= 1.0);
-    assert_float_eq!(&ma, &mb, abs_all <= 1.0);
+fn refs_debug_float_eq() {
+    let a = &1.0f32;
+    let b = &2.0f32;
+    let mut ma = &mut 1.0f32;
+    let mut mb = &mut 2.0f32;
 
-    let a = 1.0;
-    assert_float_eq!(a, mb, abs <= 1.0);
-    assert_float_eq!(a, mb, abs_all <= 1.0);
-    //assert_float_eq!(&a, mb, abs <= 1.0);
-    //assert_float_eq!(&a, mb, abs_all <= 1.0);
-    //assert_float_eq!(a, &mb, abs <= 1.0);
-    //assert_float_eq!(a, &mb, abs_all <= 1.0);
-    assert_float_eq!(&a, &mb, abs <= 1.0);
-    assert_float_eq!(&a, &mb, abs_all <= 1.0);
+    assert_eq!(FloatEqDebug::debug_abs_epsilon(&a, &b, &0.5), 0.5);
+    assert_eq!(FloatEqDebug::debug_abs_epsilon(&mut ma, &b, &0.5), 0.5);
+    assert_eq!(FloatEqDebug::debug_abs_epsilon(&a, &mut mb, &0.5), 0.5);
+    assert_eq!(FloatEqDebug::debug_abs_epsilon(&mut ma, &mut mb, &0.5), 0.5);
 
-    let b = 2.0;
-    assert_float_eq!(ma, b, abs <= 1.0);
-    assert_float_eq!(ma, b, abs_all <= 1.0);
-    //assert_float_eq!(&ma, mb, abs <= 1.0);
-    //assert_float_eq!(&ma, mb, abs_all <= 1.0);
-    //assert_float_eq!(ma, &mb, abs <= 1.0);
-    //assert_float_eq!(ma, &mb, abs_all <= 1.0);
-    assert_float_eq!(&ma, &b, abs <= 1.0);
-    assert_float_eq!(&ma, &b, abs_all <= 1.0);
-}
+    assert_eq!(FloatEqAllDebug::debug_abs_all_epsilon(&a, &b, &0.5), 0.5);
+    assert_eq!(
+        FloatEqAllDebug::debug_abs_all_epsilon(&mut ma, &b, &0.5),
+        0.5
+    );
+    assert_eq!(
+        FloatEqAllDebug::debug_abs_all_epsilon(&a, &mut mb, &0.5),
+        0.5
+    );
+    assert_eq!(
+        FloatEqAllDebug::debug_abs_all_epsilon(&mut ma, &mut mb, &0.5),
+        0.5
+    );
 
-#[test]
-#[allow(unused_mut)]
-fn mutable_refs() {
-    let mut a = 1.0;
-    let mut b = 2.0;
-    assert_float_eq!(&mut a, &b, abs <= 1.0);
-    assert_float_eq!(&mut a, &b, abs_all <= 1.0);
-    assert_float_eq!(&a, &mut b, abs <= 1.0);
-    assert_float_eq!(&a, &mut b, abs_all <= 1.0);
-    assert_float_eq!(&mut a, &mut b, abs <= 1.0);
-    assert_float_eq!(&mut a, &mut b, abs_all <= 1.0);
+    assert_eq!(FloatEqDebug::debug_rel_epsilon(&a, &b, &0.1), 0.2);
+    assert_eq!(FloatEqDebug::debug_rel_epsilon(&mut ma, &b, &0.1), 0.2);
+    assert_eq!(FloatEqDebug::debug_rel_epsilon(&a, &mut mb, &0.1), 0.2);
+    assert_eq!(FloatEqDebug::debug_rel_epsilon(&mut ma, &mut mb, &0.1), 0.2);
+
+    assert_eq!(FloatEqAllDebug::debug_rel_all_epsilon(&a, &b, &0.1), 0.2);
+    assert_eq!(
+        FloatEqAllDebug::debug_rel_all_epsilon(&mut ma, &b, &0.1),
+        0.2
+    );
+    assert_eq!(
+        FloatEqAllDebug::debug_rel_all_epsilon(&a, &mut mb, &0.1),
+        0.2
+    );
+    assert_eq!(
+        FloatEqAllDebug::debug_rel_all_epsilon(&mut ma, &mut mb, &0.1),
+        0.2
+    );
+
+    assert_eq!(FloatEqDebug::debug_ulps_epsilon(&a, &b, &1), 1);
+    assert_eq!(FloatEqDebug::debug_ulps_epsilon(&mut ma, &b, &1), 1);
+    assert_eq!(FloatEqDebug::debug_ulps_epsilon(&a, &mut mb, &1), 1);
+    assert_eq!(FloatEqDebug::debug_ulps_epsilon(&mut ma, &mut mb, &1), 1);
+
+    assert_eq!(FloatEqAllDebug::debug_ulps_all_epsilon(&a, &b, &1), 1);
+    assert_eq!(FloatEqAllDebug::debug_ulps_all_epsilon(&mut ma, &b, &1), 1);
+    assert_eq!(FloatEqAllDebug::debug_ulps_all_epsilon(&a, &mut mb, &1), 1);
+    assert_eq!(
+        FloatEqAllDebug::debug_ulps_all_epsilon(&mut ma, &mut mb, &1),
+        1
+    );
 }
