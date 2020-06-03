2810f1f5b30953c967b7c9e06aa69f631b51856d
jtempest <54986678+jtempest@users.noreply.github.com>
2020-06-03 09:32:05 +0100

    Trait impls for Box

--- tests/unit_tests/std_types.rs
+++ tests/unit_tests/std_types.rs
@@ -1,9 +1,8 @@
 use float_eq::{float_eq, FloatDiff, FloatEqAllDebug, FloatEqDebug};
-use std::rc::Rc;
-use std::sync::Arc;
 
 mod rc {
     use super::*;
+    use std::rc::Rc;
 
     #[test]
     fn float_diff() {
@@ -47,6 +46,7 @@ mod rc {
 }
 mod arc {
     use super::*;
+    use std::sync::Arc;
 
     #[test]
     fn float_diff() {
@@ -77,6 +77,51 @@ mod arc {
     fn float_eq_debug() {
         let a = Arc::new([1.0f32, 2.0]);
         let b = Arc::new([1.5f32, 2.25]);
+
+        assert_eq!(a.debug_abs_epsilon(&b, &[0.1, 0.2]), [0.1, 0.2]);
+        assert_eq!(a.debug_abs_all_epsilon(&b, &0.2), [0.2, 0.2]);
+
+        assert_eq!(a.debug_rel_epsilon(&b, &[0.1, 0.5]), [0.15, 1.125]);
+        assert_eq!(a.debug_rel_all_epsilon(&b, &0.5), [0.75, 1.125]);
+
+        assert_eq!(a.debug_ulps_epsilon(&b, &[1, 2]), [1, 2]);
+        assert_eq!(a.debug_ulps_all_epsilon(&b, &2), [2, 2]);
+    }
+}
+
+mod r#box {
+    use super::*;
+    use std::boxed::Box;
+
+    #[test]
+    fn float_diff() {
+        let a = Box::new([1.0f32, 2.0]);
+        let b = Box::new([1.5f32, 2.25]);
+        assert_eq!(a.abs_diff(&b), [0.5, 0.25]);
+
+        let c = Box::new([1.000_000_1f32, 2.000_000_5]);
+        assert_eq!(a.ulps_diff(&c), Some([1, 2]));
+    }
+
+    #[test]
+    fn float_eq() {
+        let a = Box::new([1.0f32, 2.0]);
+        let b = Box::new([1.5f32, 2.25]);
+        assert!(float_eq!(a, b, abs <= [0.5, 0.25]));
+        assert!(float_eq!(a, b, abs_all <= 0.5));
+
+        let c = Box::new([1.000_000_1f32, 2.000_000_5]);
+        assert!(float_eq!(a, c, rel <= [f32::EPSILON, 2.0 * f32::EPSILON]));
+        assert!(float_eq!(a, c, rel_all <= 2.0 * f32::EPSILON));
+
+        assert!(float_eq!(a, c, ulps <= [1, 2]));
+        assert!(float_eq!(a, c, ulps_all <= 2));
+    }
+
+    #[test]
+    fn float_eq_debug() {
+        let a = Box::new([1.0f32, 2.0]);
+        let b = Box::new([1.5f32, 2.25]);
 
         assert_eq!(a.debug_abs_epsilon(&b, &[0.1, 0.2]), [0.1, 0.2]);
         assert_eq!(a.debug_abs_all_epsilon(&b, &0.2), [0.2, 0.2]);
