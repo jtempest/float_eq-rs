bc6b5c382e26539145fe178d9e4994d27c992a48
jtempest <54986678+jtempest@users.noreply.github.com>
2021-07-21 11:00:26 +0100

    Only use badges in README, not for crates.io

--- generate-readme.bat
+++ generate-readme.bat
@@ -5,7 +5,11 @@ echo     THIS FILE IS GENERATED FROM crates-io.md AND LICENSE.md.>> README.md
 echo     DO NOT EDIT IT DIRECTLY.>> README.md
 echo --^>>> README.md
 echo.>> README.md
-type "float_eq\crates-io.md" >> README.md
+echo # float_eq >> README.md
+echo.>> README.md
+type badges.md >> README.md
+echo.>> README.md
+more +2 "float_eq\crates-io.md" >> README.md
 echo.>> README.md
 echo ^<br^>>> README.md
 echo.>> README.md
