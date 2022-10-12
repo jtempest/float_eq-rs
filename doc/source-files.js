25c80b9209d51c0e6641f0d0ca1e6df0e9a3f3f0
jtempest <54986678+jtempest@users.noreply.github.com>
2022-10-12 08:33:02 +0100

    docs deployment

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,4 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["float_eq"] = {"name":"","dirs":[{"name":"trait_impls","files":["arrays.rs","core_types.rs","num_complex.rs","primitives.rs","std_types.rs","tuples.rs"]}],"files":["lib.rs","macros.rs","trait_impls.rs","traits.rs"]};
+var sourcesIndex = JSON.parse('{\
+"float_eq":["",[["trait_impls",[],["arrays.rs","core_types.rs","num_complex.rs","primitives.rs","std_types.rs","tuples.rs"]]],["lib.rs","macros.rs","trait_impls.rs","traits.rs"]]\
+}');
 createSourceSidebar();
